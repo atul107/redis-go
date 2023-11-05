bcdca8620d6c6131f17b3d2533d80fb42a00320f
atul107 <kr.atul.atk@gmail.com>
2023-11-05 22:15:05 +0530

    Fix zrange issues

--- internal/handlers/zrange.go
+++ internal/handlers/zrange.go
@@ -18,20 +18,6 @@ func ZrangeHandler(value Value, aof *Aof) Value {
 
 	setName := args[0].Bulk
 
-	var byScore bool = true
-	if len(args) > 3 {
-		if args[3].Bulk == "BYSCORE" {
-			byScore = true
-		} else if args[3].Bulk == "BYLEX" {
-			byScore = false
-		}
-	}
-
-	rev := false
-	if len(args) > 4 && args[4].Bulk == "REV" {
-		rev = true
-	}
-
 	SortedSetStoreLock.RLock()
 	set, ok := SortedSetStore[setName]
 	SortedSetStoreLock.RUnlock()
@@ -43,11 +29,33 @@ func ZrangeHandler(value Value, aof *Aof) Value {
 	startIndexStr := args[1].Bulk
 	endIndexStr := args[2].Bulk
 
+	excludeLeft := false
+	if startIndexStr[0] == '(' {
+		excludeLeft = true
+		startIndexStr = startIndexStr[1:]
+	}
+
+	excludeRight := false
+	if endIndexStr[0] == '(' {
+		excludeRight = true
+		endIndexStr = endIndexStr[1:]
+	}
+
 	start, err := strconv.Atoi(startIndexStr)
 	if err != nil {
 		return Value{Typ: "error", Str: "ERR invalid LIMIT offset"}
 	}
 
+	if start < 0 {
+		start = len(set.members) + start
+	}
+	if start < 0 {
+		start = 0
+	}
+
+	if excludeLeft {
+		start = start + 1
+	}
 	end, err := strconv.Atoi(endIndexStr)
 	if err != nil {
 		return Value{Typ: "error", Str: "ERR invalid LIMIT count"}
@@ -56,19 +64,52 @@ func ZrangeHandler(value Value, aof *Aof) Value {
 	if end < 0 {
 		end = len(set.members) + end
 	}
+	if end < 0 {
+		end = 0
+	}
 	if end > len(set.members)-1 {
 		end = len(set.members) - 1
 	}
+
+	if excludeRight {
+		end = end - 1
+	}
+
 	memberScores := make([]struct {
 		member string
 		score  float64
 	}, end-start+1)
 
+	memberScoresIdx := 0
 	for i := start; i <= end; i++ {
-		memberScores[i] = struct {
+		memberScores[memberScoresIdx] = struct {
 			member string
 			score  float64
 		}{set.members[i], set.scores[i]}
+		memberScoresIdx++
+	}
+
+	var byScore, rev, withScores, limitReq bool
+	var offsetStr, countStr string
+	byScore = true
+	for i := 3; i < len(args); i++ {
+		arg := args[i].Bulk
+
+		if arg == "BYSCORE" {
+			byScore = true
+		} else if arg == "BYLEX" {
+			byScore = false
+		} else if arg == "REV" {
+			rev = true
+		} else if arg == "WITHSCORES" {
+			withScores = true
+		} else if arg == "LIMIT" {
+			limitReq = true
+			offsetStr = args[i+1].Bulk
+			i++
+			countStr = args[i+1].Bulk
+			i++
+		}
 	}
 
 	if byScore {
@@ -77,14 +118,7 @@ func ZrangeHandler(value Value, aof *Aof) Value {
 		sortByMember(memberScores, rev)
 	}
 
-	if len(args) > 5 && args[5].Bulk == "LIMIT" {
-		if len(args) < 8 {
-			return Value{Typ: "error", Str: "ERR wrong number of arguments for 'zrange' command with 'LIMIT'"}
-		}
-
-		offsetStr := args[6].Bulk
-		countStr := args[7].Bulk
-
+	if limitReq {
 		offset, err := strconv.Atoi(offsetStr)
 		if err != nil {
 			return Value{Typ: "error", Str: "ERR invalid LIMIT offset"}
@@ -114,21 +148,33 @@ func ZrangeHandler(value Value, aof *Aof) Value {
 		memberScores = memberScores[offset:endIndex]
 	}
 
-	members := make([]Value, len(memberScores))
-	for i, ms := range memberScores {
-		if byScore {
-			memberValue := Value{Typ: "bulk", Bulk: ms.member}
-			if rev {
-				members[i] = Value{Typ: "bulk", Bulk: strconv.FormatFloat(ms.score, 'f', -1, 64)}
-			} else {
-				members[i] = memberValue
-			}
-		} else {
-			members[i] = Value{Typ: "bulk", Bulk: ms.member}
+	if withScores {
+		membersWithScores := make([]Value, len(memberScores)*2)
+		for i, ms := range memberScores {
+			// if byScore {
+			membersWithScores[i*2] = Value{Typ: "bulk", Bulk: ms.member}
+			membersWithScores[i*2+1] = Value{Typ: "bulk", Bulk: strconv.FormatFloat(ms.score, 'f', -1, 64)}
+			// } else {
+			// 	membersWithScores[i*2] = Value{Typ: "bulk", Bulk: ms.member}
+			// }
 		}
+		return Value{Typ: "array", Array: membersWithScores}
+	} else {
+		members := make([]Value, len(memberScores))
+		for i, ms := range memberScores {
+			// if byScore {
+			// 	memberValue := Value{Typ: "bulk", Bulk: ms.member}
+			// 	if rev {
+			// 		members[i] = Value{Typ: "bulk", Bulk: strconv.FormatFloat(ms.score, 'f', -1, 64)}
+			// 	} else {
+			// 		members[i] = memberValue
+			// 	}
+			// } else {
+			members[i] = Value{Typ: "bulk", Bulk: ms.member}
+			// }
+		}
+		return Value{Typ: "array", Array: members}
 	}
-
-	return Value{Typ: "array", Array: members}
 }
 
 func sortByScore(memberScores []struct {
